e82c63624b9b058c71486572a40b2d44f5cc13fa
Daniel Prates <danprates94@gmail.com>
2021-11-04 22:40:28 -0300

    chore: add dotenv config

--- src/main/config/env.config.ts
+++ src/main/config/env.config.ts
@@ -1,3 +1,7 @@
+import { config } from 'dotenv'
+
+config({ path: process.env.NODE_ENV === 'test' ? '.env.testing' : '.env' })
+
 export const {
   PORT = 3000,
   API_VERSION = 'v1',
--- src/main/routes/health.routes.integration.ts
+++ src/main/routes/health.routes.integration.ts
@@ -1,11 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
+import { API_VERSION, NODE_ENV } from '../config/env.config'
 
 describe('/health routes', () => {
   it('Should return health message', async () => {
     await request(app)
-      .get('/api/v1/health')
+      .get(`/api/${API_VERSION}/health`)
       .expect(200)
-      .expect({ message: 'App is running in test mode' })
+      .expect({ message: `App is running in ${NODE_ENV} mode` })
   })
 })
