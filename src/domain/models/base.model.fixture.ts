cc8ce6ee0f15f35f2e6974634420910b217f2ec0
Daniel Prates <danprates94@gmail.com>
2021-11-07 16:11:00 -0300

    feat: add isActive and isDeleted in BaseModel

--- src/domain/models/base.model.fixture.ts
+++ src/domain/models/base.model.fixture.ts
@@ -3,6 +3,8 @@ import { BaseModel } from './base.model'
 
 export const BaseModelFixture = (): BaseModel => ({
   id: faker.datatype.uuid(),
+  isActive: faker.datatype.boolean(),
+  isDeleted: faker.datatype.boolean(),
   createdAt: faker.date.recent(10),
   updatedAt: new Date()
 })
--- src/domain/models/base.model.ts
+++ src/domain/models/base.model.ts
@@ -1,5 +1,7 @@
 export interface BaseModel {
   id: string
+  isActive: boolean
+  isDeleted: boolean
   createdAt: Date
   updatedAt: Date
 }
