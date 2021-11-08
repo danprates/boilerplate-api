66152f8a91ff9895481d759923e3cda61b8fdded
Daniel Prates <danprates94@gmail.com>
2021-11-07 21:11:28 -0300

    fix: import file in routes.config

--- src/main/config/routes.config.ts
+++ src/main/config/routes.config.ts
@@ -1,6 +1,5 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-
 import { API_VERSION } from './env.config'
 
 export const routesConfig = (app: Express): void => {
@@ -8,7 +7,7 @@ export const routesConfig = (app: Express): void => {
   app.use(`/api/${API_VERSION}`, router)
   // eslint-disable-next-line node/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async (file) => {
-    if (file.endsWith('.routes.ts')) {
+    if (!file.endsWith('.map') && !file.includes('.integration.ts')) {
       ;(await import(`../routes/${file}`)).default(router)
     }
   })
