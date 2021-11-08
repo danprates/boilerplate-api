e9cc7e50b7d2b71bac582acb8d2115eadd2ca599
Daniel Prates <danprates94@gmail.com>
2021-11-07 23:04:43 -0300

    fix: add name of table

--- src/infra/databases/postgres/entities/user.entity.ts
+++ src/infra/databases/postgres/entities/user.entity.ts
@@ -1,6 +1,7 @@
-import { Column } from 'typeorm'
+import { Column, Entity } from 'typeorm'
 import { AuthEntity } from './auth.entity'
 
+@Entity('users')
 export class UserEntity extends AuthEntity {
   @Column({ nullable: false })
   name: string
