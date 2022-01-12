abbbce2ec61f8cefc13d6738708a97fa5ce7aa72
Daniel Prates <danprates94@gmail.com>
2022-01-11 23:37:00 -0300

    fix: remove secret from auth.fixture

--- src/domain/models/auth.model.fixture.ts
+++ src/domain/models/auth.model.fixture.ts
@@ -5,6 +5,6 @@ import { AuthModel } from './auth.model'
 export const AuthModelFixture = (): AuthModel => ({
   ...BaseModelFixture(),
   email: faker.internet.email(),
-  password: 'secret',
+  password: faker.internet.password(),
   passwordResetToken: faker.datatype.uuid()
 })
