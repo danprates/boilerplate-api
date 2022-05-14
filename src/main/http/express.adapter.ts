dfabb142b379ac7b426312d4b529df203590de95
Daniel Prates <danprates94@gmail.com>
2022-05-13 21:06:05 -0300

    feat: add controller adapter to express adapter

--- src/main/http/express.adapter.ts
+++ src/main/http/express.adapter.ts
@@ -1,22 +1,52 @@
-import { Controller } from '@/application/protocols'
-import express from 'express'
-import { expressRouteAdapter } from '../adapters/express-route.adapter'
+import { Controller, HttpRequest } from '@/application/protocols'
+import express, { Express, json, Request, Response } from 'express'
+import { API_VERSION } from '../config/env.config'
 import { Http } from './http.protocol'
 
 export default class ExpressAdapter implements Http {
-  app: express.Express
+  app: Express
 
   constructor() {
     this.app = express()
-    this.app.use(express.json())
+    this.app.use(json())
   }
 
   on(method: string, url: string, factory: () => Controller): void {
-    this.app[method]('/api/v1' + url, expressRouteAdapter(factory()))
+    this.app[method](
+      `/api/${API_VERSION}/${url}`,
+      this.controllerAdapter(factory())
+    )
   }
 
   listen(port: number, callback: any): void {
     this.app.listen(port)
     callback()
+  }
+
+  controllerAdapter(controller: Controller): any {
+    return async (req: Request, res: Response) => {
+      try {
+        const httpRequest: HttpRequest = {
+          body: req.body,
+          params: req.params
+        }
+
+        if (req.query) {
+          const { take, skip, ...query } = req.query
+
+          httpRequest.query = {
+            take: Number(req.query.take) || 10,
+            skip: Number(req.query.skip) || 0,
+            ...query
+          }
+        }
+
+        const httpResponse = await controller.handler(httpRequest)
+
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+      } catch (error) {
+        res.status(500).json({ message: 'Server erro', code: error.name })
+      }
+    }
   }
 }
--- src/main/http/http.protocol.ts
+++ src/main/http/http.protocol.ts
@@ -3,4 +3,5 @@ import { Controller } from '@/application/protocols'
 export interface Http {
   on: (method: string, url: string, factory: () => Controller) => void
   listen: (port: number, callback: any) => void
+  controllerAdapter: (controller: Controller) => any
 }
