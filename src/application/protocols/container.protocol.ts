0944d65169a4e2e5544560ae6f7cd1b3fc2bc19f
Daniel Prates <danprates94@gmail.com>
2022-08-10 18:59:33 -0300

    feat: add more actions to Logger protocol

--- src/application/protocols/container.protocol.ts
+++ src/application/protocols/container.protocol.ts
@@ -2,6 +2,9 @@ export declare namespace Dependencies {
   export interface Logger {
     debug: (message: string, values?: any) => void
     info: (message: string, values?: any) => void
+    warn: (message: string, object?: any) => void
+    error: (message: string, object?: any) => void
+    fatal: (message: string, object?: any) => void
   }
 
   export interface Container {
