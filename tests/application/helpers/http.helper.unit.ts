b72803a0090fe5f13c525d88efb3849db81872e1
Daniel Prates <danprates94@gmail.com>
2022-08-11 13:02:29 -0300

    test: implement Response interface to http helpers

--- tests/application/helpers/http.helper.unit.ts
+++ tests/application/helpers/http.helper.unit.ts
@@ -9,43 +9,43 @@ import { ErrorModel } from '@/application/models'
 
 describe('HTTPHelper', () => {
   it('should return correct values when ok is called', () => {
-    expect(ok('any_data')).toEqual({ body: 'any_data', statusCode: 200 })
+    expect(ok('any_data')).toEqual({ data: 'any_data', statusCode: 200 })
   })
 
   it('should return correct values when created is called', () => {
-    expect(created('any_data')).toEqual({ body: 'any_data', statusCode: 201 })
+    expect(created('any_data')).toEqual({ data: 'any_data', statusCode: 201 })
   })
 
   it('should return bad request when ErrorModel.invalidParams is called', () => {
     let err = ErrorModel.invalidParams('any_error')
 
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 400
     })
 
     err = ErrorModel.invalidParams()
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 400
     })
   })
 
   it('should return correct values when noContent is called', () => {
-    expect(noContent()).toEqual({ body: null, statusCode: 204 })
+    expect(noContent()).toEqual({ data: null, statusCode: 204 })
   })
 
   it('should return correct values when unauthorized is called', () => {
     let err = ErrorModel.unauthorized('any_error')
 
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 401
     })
 
     err = ErrorModel.unauthorized()
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 401
     })
   })
@@ -53,27 +53,27 @@ describe('HTTPHelper', () => {
   it('should return correct values when notFound is called', () => {
     let err = ErrorModel.notFound('any_error')
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 404
     })
 
     err = ErrorModel.notFound()
     expect(resultErrorHandler(err)).toEqual({
-      body: { message: err.message },
+      data: { message: err.message },
       statusCode: 404
     })
   })
 
   it('should return correct values when serverError is called', () => {
     expect(serverError()).toEqual({
-      body: { message: 'Server error' },
+      data: { message: 'Server error' },
       statusCode: 500
     })
   })
 
   it('should return default values when resultErrorHandler is called without any error', () => {
     expect(resultErrorHandler()).toEqual({
-      body: { message: 'Unexpected error' },
+      data: { message: 'Unexpected error' },
       statusCode: 500
     })
   })
