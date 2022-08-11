3c0e431ca61165795d5dab5fbf625d0f417d4b2c
Daniel Prates <danprates94@gmail.com>
2022-08-11 16:44:44 -0300

    refactor: validator to receive Domain.Request in params

--- src/application/protocols/validator.protocol.ts
+++ src/application/protocols/validator.protocol.ts
@@ -1,5 +1,6 @@
 import { Result } from '@/application/models'
+import { Domain } from './domain.protocol'
 
-export interface Validator<T = any> {
-  run: (data: any) => Result<T>
+export interface Validator {
+  check: (data: Domain.Request, schemaName: string) => Result<Domain.Request>
 }
