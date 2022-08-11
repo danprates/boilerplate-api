7b33d8b9e790181fa2725d19a3c62c3774519061
Daniel Prates <danprates94@gmail.com>
2022-08-11 16:48:57 -0300

    feat: add schemas to joi adapter

--- src/infra/validators/joi.adapter.ts
+++ src/infra/validators/joi.adapter.ts
@@ -1,17 +1,43 @@
 import { ErrorModel, Result } from '@/application/models'
-import { Validator } from '@/application/protocols'
+import { Domain, Validator } from '@/application/protocols'
+import { readdirSync } from 'fs'
 import Joi from 'joi'
+import { join } from 'path'
+import BaseSchema from './base-schema'
 
-export class JoiAdapter<T = any> implements Validator {
-  constructor(private readonly schema: Joi.Schema) {}
+export class JoiAdapter implements Validator {
+  private readonly schemas: BaseSchema[] = []
+  constructor() {
+    this.initSchemas()
+  }
 
-  run(data: any): Result<T> {
-    const { error, value } = this.schema.validate(data, { convert: true })
+  check(data: Domain.Request, schemaName: string): Result<Domain.Request> {
+    const schema = this.getSchema(schemaName)
+    const { error, value } = schema.validate(data, { convert: true })
 
     if (error) {
       return Result.fail(ErrorModel.invalidParams(error.message))
     }
 
     return Result.ok(value)
+  }
+
+  getSchema(name: string): Joi.Schema {
+    const schema = this.schemas.find((schema) => schema.name)
+    if (!schema) throw new Error(`${name} schema not found`)
+    return schema.getSchema()
+  }
+
+  initSchemas(): void {
+    const path = join(__dirname, './schemas')
+
+    readdirSync(path).forEach(async (file) => {
+      const validFile = file.endsWith('.schema.ts')
+
+      if (validFile) {
+        const Schema = (await import(`${path}/${file}`)).default
+        this.schemas.push(new Schema())
+      }
+    })
   }
 }
