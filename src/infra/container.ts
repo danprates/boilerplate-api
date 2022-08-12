d87fffda2b72cb089c9b16265d75fd8b55f40bf2
Daniel Prates <danprates94@gmail.com>
2022-08-12 17:18:56 -0300

    feat: add repository and validation to container

--- src/infra/container.ts
+++ src/infra/container.ts
@@ -1,6 +1,10 @@
 import { Dependencies } from '@/application/protocols'
+import { BaseRepository } from './databases/typeorm/repositories'
 import { PinoLoggerAdapter } from './monitoration/pino-logger.adapter'
+import { JoiAdapter } from './validators/joi.adapter'
 
 export const container: Dependencies.Container = {
-  logger: new PinoLoggerAdapter()
+  logger: new PinoLoggerAdapter(),
+  repository: new BaseRepository(null),
+  validation: new JoiAdapter()
 }
