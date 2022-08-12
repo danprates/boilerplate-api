b8f7e4e6cda125e2561fc415307890bd4eb8897a
Daniel Prates <danprates94@gmail.com>
2022-08-12 16:31:40 -0300

    test: add container fixture

---
+++ tests/infra/container.fixture.ts
@@ -0,0 +1,22 @@
+import { Dependencies } from '@/application/protocols'
+
+export const containerFixture: Dependencies.Container = {
+  logger: {
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    fatal: jest.fn()
+  },
+  validation: {
+    check: jest.fn()
+  },
+  repository: {
+    create: jest.fn(),
+    softDelete: jest.fn(),
+    hardDelete: jest.fn(),
+    find: jest.fn(),
+    list: jest.fn(),
+    update: jest.fn()
+  }
+}
