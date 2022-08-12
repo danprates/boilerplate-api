337c4c962b3a7ada7c58f717cd213c82740f8265
Daniel Prates <danprates94@gmail.com>
2022-08-12 17:15:58 -0300

    refactor: remove factories

--- src/infra/factories/users/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './create-user.factory'
-export * from './delete-user.factory'
-export * from './find-user.factory'
-export * from './list-users.factory'
-export * from './update-user.factory'
