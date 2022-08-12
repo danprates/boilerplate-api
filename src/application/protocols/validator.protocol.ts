b185b74d534a8bbb5b7dc3b184e82d3e22cc125d
Daniel Prates <danprates94@gmail.com>
2022-08-12 17:17:07 -0300

    refactor: remove validation from protocols

--- src/application/protocols/validator.protocol.ts
+++
@@ -1,6 +0,0 @@
-import { Result } from '@/application/models'
-import { Domain } from './domain.protocol'
-
-export interface Validator {
-  check: (data: Domain.Request, schemaName: string) => Result<Domain.Request>
-}
