f8ad7ecd2f8716c92f595e714e015ddee7de1513
Daniel Prates <danprates94@gmail.com>
2022-08-16 21:15:53 -0300

    feat: add addResolver to Http protocol

--- src/application/protocols/application.protocol.ts
+++ src/application/protocols/application.protocol.ts
@@ -3,6 +3,7 @@ import { Domain } from './domain.protocol'
 export declare namespace App {
   export interface Http {
     addRoute: (method: string, url: string, useCase: Domain.UseCase) => void
+    addResolver: (useCase: Domain.UseCase) => void
     listen: (port: number, callback: any) => void
   }
 }
