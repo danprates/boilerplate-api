c64ab974bc4a9b80f12ab8394adffad01b2b274c
Daniel Prates <danprates94@gmail.com>
2022-08-23 18:36:49 -0300

    test: implement useCaseName in PinoLoggerAdapter

--- tests/infra/monitoration/pino-logger.adapter.spec.ts
+++ tests/infra/monitoration/pino-logger.adapter.spec.ts
@@ -18,30 +18,42 @@ describe('PinoLoggerAdapter', () => {
   it('should call debug with correct values', () => {
     const sut = new PinoLoggerAdapter()
     sut.debug('debug_message')
-    expect(pinoMock.debug).toHaveBeenNthCalledWith(1, {}, 'debug_message')
+    expect(pinoMock.debug).toHaveBeenNthCalledWith(
+      1,
+      {},
+      '[APP]: debug_message'
+    )
   })
 
   it('should call info with correct values', () => {
     const sut = new PinoLoggerAdapter('any_name')
     sut.info('info_message')
-    expect(pinoMock.info).toHaveBeenNthCalledWith(1, {}, 'info_message')
+    expect(pinoMock.info).toHaveBeenNthCalledWith(1, {}, '[APP]: info_message')
   })
 
   it('should call warn with correct values', () => {
     const sut = new PinoLoggerAdapter('any_name')
     sut.warn('warn_message')
-    expect(pinoMock.warn).toHaveBeenNthCalledWith(1, {}, 'warn_message')
+    expect(pinoMock.warn).toHaveBeenNthCalledWith(1, {}, '[APP]: warn_message')
   })
 
   it('should call error with correct values', () => {
     const sut = new PinoLoggerAdapter('any_name')
     sut.error('error_message')
-    expect(pinoMock.error).toHaveBeenNthCalledWith(1, {}, 'error_message')
+    expect(pinoMock.error).toHaveBeenNthCalledWith(
+      1,
+      {},
+      '[APP]: error_message'
+    )
   })
 
   it('should call fatal with correct values', () => {
     const sut = new PinoLoggerAdapter('any_name')
     sut.fatal('fatal_message')
-    expect(pinoMock.fatal).toHaveBeenNthCalledWith(1, {}, 'fatal_message')
+    expect(pinoMock.fatal).toHaveBeenNthCalledWith(
+      1,
+      {},
+      '[APP]: fatal_message'
+    )
   })
 })
