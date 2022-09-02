aec68c581eb51e26d6de6309a56bcec0c35321ad
Daniel Prates <danprates94@gmail.com>
2022-09-02 17:37:51 -0300

    test: make ErrorEntity extends Error

--- tests/domain/entities/error.spec.ts
+++ tests/domain/entities/error.spec.ts
@@ -1,68 +1,47 @@
 import { ErrorCode, ErrorEntity, ErrorMessages } from '@/domain/entities/error'
 
-describe('Error model', () => {
-  it('should return notFound error', () => {
-    expect(ErrorEntity.notFound()).toEqual({
-      code: ErrorCode.NOT_FOUND,
-      message: ErrorMessages.NOT_FOUND
-    })
+describe('Error entity', () => {
+  let err: ErrorEntity
 
-    expect(ErrorEntity.notFound('any_message')).toEqual({
-      code: ErrorCode.NOT_FOUND,
-      message: 'any_message'
-    })
+  it('should return notFound error', () => {
+    err = ErrorEntity.notFound()
+    expect(err.code).toEqual(ErrorCode.NOT_FOUND)
+    expect(err.message).toEqual(ErrorMessages.NOT_FOUND)
+
+    err = ErrorEntity.notFound('any_message')
+    expect(err.message).toEqual('any_message')
   })
 
   it('should return invalidParams error', () => {
-    expect(ErrorEntity.invalidParams()).toEqual({
-      code: ErrorCode.BAD_REQUEST,
-      message: ErrorMessages.INVALID_PARAMS
-    })
+    err = ErrorEntity.invalidParams()
+    expect(err.code).toEqual(ErrorCode.BAD_REQUEST)
+    expect(err.message).toEqual(ErrorMessages.INVALID_PARAMS)
 
-    expect(ErrorEntity.invalidParams('any_message')).toEqual({
-      code: ErrorCode.BAD_REQUEST,
-      message: 'any_message'
-    })
+    err = ErrorEntity.invalidParams('any_message')
+    expect(err.message).toEqual('any_message')
   })
 
   it('should return unauthorized error', () => {
-    expect(ErrorEntity.unauthorized()).toEqual({
-      code: ErrorCode.UNAUTHORIZED,
-      message: ErrorMessages.UNAUTHORIZED
-    })
+    err = ErrorEntity.unauthorized()
+    expect(err.code).toEqual(ErrorCode.UNAUTHORIZED)
+    expect(err.message).toEqual(ErrorMessages.UNAUTHORIZED)
 
-    expect(ErrorEntity.unauthorized('any_message')).toEqual({
-      code: ErrorCode.UNAUTHORIZED,
-      message: 'any_message'
-    })
+    err = ErrorEntity.unauthorized('any_message')
+    expect(err.code).toEqual(ErrorCode.UNAUTHORIZED)
+    expect(err.message).toEqual('any_message')
   })
 
   it('should return correct error from status code', () => {
-    expect(ErrorEntity.fromStatusCode(404, 'Resource not found')).toEqual({
-      code: ErrorCode.NOT_FOUND,
-      message: 'Resource not found'
-    })
+    err = ErrorEntity.fromStatusCode(404, 'Resource not found')
+    expect(err.code).toEqual(404)
+    expect(err.message).toEqual('Resource not found')
 
-    expect(ErrorEntity.fromStatusCode(undefined)).toEqual({
-      code: ErrorCode.SERVER_ERROR,
-      message: ErrorMessages.SERVER_ERROR
-    })
+    err = ErrorEntity.fromStatusCode(undefined)
+    expect(err.code).toEqual(ErrorCode.SERVER_ERROR)
+    expect(err.message).toEqual(ErrorMessages.SERVER_ERROR)
 
-    expect(ErrorEntity.fromStatusCode(1321321321321)).toEqual({
-      code: ErrorCode.SERVER_ERROR,
-      message: ErrorMessages.SERVER_ERROR
-    })
-  })
-
-  it('should return correct ErrorEntity from Error', () => {
-    expect(ErrorEntity.fromError(new Error('some error'))).toEqual({
-      code: ErrorCode.SERVER_ERROR,
-      message: 'some error'
-    })
-
-    expect(ErrorEntity.fromError(new Error())).toEqual({
-      code: ErrorCode.SERVER_ERROR,
-      message: ErrorMessages.SERVER_ERROR
-    })
+    err = ErrorEntity.fromStatusCode(1321321321321)
+    expect(err.code).toEqual(ErrorCode.SERVER_ERROR)
+    expect(err.message).toEqual(ErrorMessages.SERVER_ERROR)
   })
 })
